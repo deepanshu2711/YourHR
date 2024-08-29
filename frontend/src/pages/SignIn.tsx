3da1dec23c3c82836242e580932c6736f3313be7
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-08-29 11:06:44 +0530

    initialize frontend and created signin and signup page UI

---
+++ frontend/src/pages/SignIn.tsx
@@ -0,0 +1,43 @@
+import { Link } from "react-router-dom";
+
+const SignIn = () => {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center">
+      <div className="border p-5 flex flex-col gap-8 md: min-w-[400px] rounded-md shadow-sm">
+        <div className="flex flex-col">
+          <p className="text-2xl font-semibold">Sign In</p>
+          <p className="text-gray-600 text-sm">Sign in to your account</p>
+        </div>
+        <hr />
+        <form className="flex flex-col gap-3">
+          <input
+            type="text"
+            placeholder="Email"
+            className="p-2 border rounded-md"
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            className="p-2 border rounded-md"
+          />
+          <button className="w-full hover:bg-gray-900 bg-gray-950 text-white text-center uppercase p-2 rounded-md mt-2">
+            Sign Up
+          </button>
+        </form>
+        <div className="text-gray-500 text-sm">
+          <p>
+            Have account?{" "}
+            <Link
+              to={"/signup"}
+              className="text-black font-bold cursor-pointer"
+            >
+              sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignIn;
