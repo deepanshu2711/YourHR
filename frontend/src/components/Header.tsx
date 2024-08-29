5e85395282b2f47fd2502c2144c9bd82a03c50af
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-08-29 11:39:12 +0530

    initialize backend

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,7 +1,7 @@
 const Header = () => {
   return (
     <div className="flex items-center justify-between max-w-7xl mx-auto py-5">
-      <p className="text-3xl font-bold text-purple-500">Your HR</p>
+      <p className="text-3xl font-bold text-purple-500">YourHR</p>
       <div className="flex items-center gap-2">
         <p className="text-sm font-semibold text-gray-500">@Deepanshu</p>
         <img src="/user.png" className="h-8 w-8" />
